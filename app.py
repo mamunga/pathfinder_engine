6ba2e160e7498a1f2393bfc2fd836603bdcc554d
Zoltan Kupa <zoltan.kupa@gmail.com>
2021-02-01 18:22:09 +0100

    Migrated to Flask

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, json
 import os
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+data_files = ['alignments.json', 'classes.json', 'races.json', 'stats.json', 'skills.json']
+data = dict()
+
+for data_file in data_files:
+    json_url = os.path.join("static/data", data_file)
+    data = {**data, **(json.load(open(json_url)))}
+
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    print(data)
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run()
